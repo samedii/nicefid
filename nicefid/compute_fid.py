9a97f0453fc99d9ca325d073aaa8faffb60eef30
Richard Löwenström <samedii@gmail.com>
2022-07-11 22:37:32 +0200

    fix: calculate fid from features using cleanfid implementation

--- nicefid/compute_fid.py
+++ nicefid/compute_fid.py
@@ -1,32 +1,13 @@
+import numpy as np
+from scipy import linalg
 import torch
 
 from .features import Features
+from .sqrtm_eig import sqrtm_eig
 
 
-class _MatrixSquareRootEig(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, a):
-        vals, vecs = torch.linalg.eigh(a)
-        ctx.save_for_backward(vals, vecs)
-        return vecs @ vals.abs().sqrt().diag_embed() @ vecs.transpose(-2, -1)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        vals, vecs = ctx.saved_tensors
-        d = vals.abs().sqrt().unsqueeze(-1).repeat_interleave(vals.shape[-1], -1)
-        vecs_t = vecs.transpose(-2, -1)
-        return vecs @ (vecs_t @ grad_output @ vecs / (d + d.transpose(-2, -1))) @ vecs_t
-
-
-def sqrtm_eig(a):
-    if a.ndim < 2:
-        raise ValueError("Tensor of matrices must have at least 2 dimensions")
-    if a.shape[-2] != a.shape[-1]:
-        raise ValueError("Tensor must be batches of square matrices")
-    return _MatrixSquareRootEig.apply(a)
-
-
-def fid(x, y, eps=1e-8):
+# 307 vs 310 FID compard to cleanfid
+def fid(x, y, eps=1e-6):
     x_mean = x.mean(dim=0)
     y_mean = y.mean(dim=0)
     mean_term = (x_mean - y_mean).pow(2).sum()
@@ -40,8 +21,50 @@ def fid(x, y, eps=1e-8):
     return mean_term + cov_term
 
 
+def cleanfid_fid(x: np.array, y: np.array, eps=1e-6) -> float:
+    mu1 = np.mean(x, axis=0)
+    sigma1 = np.cov(x, rowvar=False)
+    mu2 = np.mean(y, axis=0)
+    sigma2 = np.cov(y, rowvar=False)
+
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+
+    if mu1.shape != mu2.shape:
+        raise ValueError("Training and test mean vectors have different lengths")
+    if sigma1.shape != sigma2.shape:
+        raise ValueError("Training and test covariances have different dimensions")
+
+    diff = mu1 - mu2
+
+    # Product might be almost singular
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = (
+            "fid calculation produces singular product; "
+            "adding %s to diagonal of cov estimates"
+        ) % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    # Numerical error might give slight imaginary component
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError("Imaginary component {}".format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return diff.dot(diff) + np.trace(sigma1) + np.trace(sigma2) - 2 * tr_covmean
+
+
 def compute_fid(a: Features, b: Features) -> float:
-    return fid(a.features, b.features)
+    # return fid(a.features, b.features)
+    return cleanfid_fid(a.features.cpu().numpy(), b.features.cpu().numpy())
 
 
 def test_fid_directories():
@@ -53,4 +76,6 @@ def test_fid_directories():
 
     features_a = Features.from_directory("tests/pixelart/dataset_a")
     features_b = Features.from_directory("tests/pixelart/dataset_b")
-    assert compute_fid(features_a, features_b) == reference_fid_score
+    assert np.allclose(
+        compute_fid(features_a, features_b), reference_fid_score, atol=1e-2
+    )
